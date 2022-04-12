7b26614ee7c53e012cbf89dcc8cde4774c218ad1
PINAK DATTA <pinakdatta30402@gmail.com>
2022-04-13 00:45:13 +0530

    Updated Code

--- app/src/main/java/com/example/todone/MainActivity.java
+++ app/src/main/java/com/example/todone/MainActivity.java
@@ -4,22 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editTextTask = findViewById(R.id.editTextTask);
+        Button addTaskBtn = findViewById(R.id.addTaskBtn);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        addTaskBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                String newTask = editTextTask.getText().toString();
+
+                if (newTask.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please add a task first.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                final TaskModel taskModel = new TaskModel();
+                taskModel.setTaskDone(false);
+                taskModel.setTask(newTask);
+
+                db.collection("todoCollection")
+                        .add(taskModel)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                editTextTask.setText(null);
+                            }
+                        });
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/todone/TaskModel.java
+++ app/src/main/java/com/example/todone/TaskModel.java
@@ -31,4 +31,4 @@ public class TaskModel {
         isTaskDone = taskDone;
     }
 }
-}
+
