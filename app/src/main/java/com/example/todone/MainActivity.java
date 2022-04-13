84ba5a9e1e7ddb39749b7f0623d54884d4f43683
PINAK DATTA <pinakdatta30402@gmail.com>
2022-04-13 10:00:55 +0530

    Added Model

--- app/src/main/java/com/example/todone/MainActivity.java
+++ app/src/main/java/com/example/todone/MainActivity.java
@@ -1,51 +1,27 @@
 package com.example.todone;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText editTextTask = findViewById(R.id.editTextTask);
-        Button addTaskBtn = findViewById(R.id.addTaskBtn);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        addTaskBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        getSupportActionBar().hide();
 
-                String newTask = editTextTask.getText().toString();
+        tasksRecyclerView = findViewById(R.id.tasksRv);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-                if (newTask.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please add a task first.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
 
-                final TaskModel taskModel = new TaskModel();
-                taskModel.setTaskDone(false);
-                taskModel.setTask(newTask);
-
-                db.collection("todoCollection")
-                        .add(taskModel)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                editTextTask.setText(null);
-                            }
-                        });
-            }
-        });
 
     }
 
